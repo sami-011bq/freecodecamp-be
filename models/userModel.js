ef6ccc02918f8522ae859d057857896863859497
sami-bytequest <samiullah.bytequests@gmail.com>
2024-09-07 19:31:08 +0530

    added fields validation

--- models/userModel.js
+++ models/userModel.js
@@ -4,11 +4,13 @@ const userSchema = new Schema(
   {
     name: {
       type: String,
+      required: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
     },
     password: {
       type: String,
